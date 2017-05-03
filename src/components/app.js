db5438b1f5897fe7f24def89180d0600d0de5eca
Peter Kang <peter.kang@cmlmediasoft.local>
2017-05-03 08:48:07 -0700

    get info from live api

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import SearchBar from './SearchBar/SearchBar';
+import searchSpotify from '../utils/searchSpotify';
 
 export default class App extends Component {
 
@@ -6,13 +8,21 @@ export default class App extends Component {
     super(props);
     this.state = {
       initialMessage: 'greeting',
+      song: '',
     };
   }
 
+  fetchSongs = () => {
+    searchSpotify(this.state.song)
+      .then((tracks) => console.log('tracks', tracks));
+  }
+
   render() {
-    const { initialMessage } = this.state;
+    const { initialMessage, song } = this.state;
+
     return (
       <div>
+        <SearchBar updateText={(song) => this.setState({ song })} fetchSongs={this.fetchSongs} />
         {initialMessage}
       </div>
     );
