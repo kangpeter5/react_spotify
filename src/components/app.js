8eb1a4395865a4579c3fbbdf4c75cc36cd70f1d5
Peter Kang <peter.kang@cmlmediasoft.local>
2017-05-03 14:12:05 -0700

    added css

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar/SearchBar';
 import searchSpotify from '../utils/searchSpotify';
+import SongItem from './SongItem/SongItem';
+import styles from './app.css';
 
 export default class App extends Component {
 
@@ -9,21 +11,25 @@ export default class App extends Component {
     this.state = {
       initialMessage: 'greeting',
       song: '',
+      tracks: {},
     };
   }
 
   fetchSongs = () => {
     searchSpotify(this.state.song)
-      .then((tracks) => console.log('tracks', tracks));
+      // { tracks: tracks } = { tracks } since the value's and key's names
+      // are the same you can get rid of the value and just keep the key
+      .then(({ tracks }) => this.setState({ tracks }));
   }
 
   render() {
-    const { initialMessage, song } = this.state;
+    const { initialMessage, song, tracks } = this.state;
+    console.log ('tracks', tracks);
 
     return (
-      <div>
+      <div className={styles.root}>
         <SearchBar updateText={(song) => this.setState({ song })} fetchSongs={this.fetchSongs} />
-        {initialMessage}
+        {tracks.items && <SongItem songData={tracks.items[0]} />}
       </div>
     );
   }
